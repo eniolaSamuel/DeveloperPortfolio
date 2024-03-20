f775d20990f014eb95067a4a90d02824127853d2
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-20 22:56:26 +0100

    tech update

--- src/views/components/Technologies.jsx
+++ src/views/components/Technologies.jsx
@@ -1,10 +1,9 @@
 import JavascriptIcon from '../../assets/svg/icons8-javascript.svg';
 import React from "react";
-import ARView from "./ARView";
 
 function Technologies() {
     return (
-        <div>
+        <div className="tech-main-frame">
 
         </div>
     );
