0916c984dee2d926ccb84d3852ddb7701d80b69d
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-22 09:37:03 +0100

    SKILLS update

--- src/views/components/Technologies.jsx
+++ src/views/components/Technologies.jsx
@@ -1,11 +1,19 @@
 import JavascriptIcon from '../../assets/svg/icons8-javascript.svg';
 import React from "react";
+import Html from '../../assets/svg/icons8-html.svg';
+
 
 function Technologies() {
     return (
         <div className="tech-main-frame">
             <h1>MY SKILLS</h1>
             <p>Technologies</p>
+            <div>
+                <div><img src={Html} alt={Html}/> </div>
+                <div><img src={Html} alt={Html}/> </div>
+                <div><img src={JavascriptIcon} alt={JavascriptIcon}/> </div>
+
+            </div>
 
         </div>
     );
