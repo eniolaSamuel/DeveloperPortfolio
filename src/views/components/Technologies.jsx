3add27054415bb028354a5b54b23da6dc3ca33e2
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-24 20:57:03 +0100

    SKILLS update

--- src/views/components/Technologies.jsx
+++ src/views/components/Technologies.jsx
@@ -2,6 +2,8 @@ import JavascriptIcon from '../../assets/svg/icons8-javascript.svg';
 import React from "react";
 import Html from '../../assets/svg/icons8-html.svg';
 import CSS from '../../assets/svg/icons8-css.svg'
+import Figma from '../../assets/svg/icons8-figma.svg'
+import MySQL from '../../assets/svg/icons8-mysql.svg'
 
 
 function Technologies() {
@@ -12,6 +14,8 @@ function Technologies() {
             <div>
                 <div><img src={Html} alt={Html}/> </div>
                 <div><img src={CSS} alt={CSS}/> </div>
+                <div><img src={JavascriptIcon} alt={JavascriptIcon}/> </div>
+                <div><img src={MySQL} alt={MySQL}/> </div>
                 <div><img src={JavascriptIcon} alt={JavascriptIcon}/> </div>
 
             </div>
