2d50af09120e26118f53316f73a7533bf3b64fa7
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-26 20:15:29 +0100

    SKILLS update

--- src/views/components/Technologies.jsx
+++ src/views/components/Technologies.jsx
@@ -5,21 +5,48 @@ import CSS from '../../assets/svg/icons8-css.svg'
 import Figma from '../../assets/svg/icons8-figma.svg'
 import MySQL from '../../assets/svg/icons8-mysql.svg'
 import "../../styles/components/Technologies.css"
+import {motion} from "framer-motion";
+import {textVariant} from "../utils/motion";
+import frontend from "../../assets/svg/frontend.svg";
 
 
 function Technologies() {
     return (
         <div className="tech-main-frame">
-            <h1>MY SKILLS</h1>
-            <p>Technologies</p>
             <div className="tech-skill-frame">
-                <div className="skill-frame"><img src={Html} alt={Html}/> </div>
-                <div className="skill-frame"><img src={CSS} alt={CSS}/> </div>
-                <div><img src={JavascriptIcon} alt={JavascriptIcon}/> </div>
-                <div><img src={MySQL} alt={MySQL}/> </div>
-                <div><img src={Figma} alt={Figma}/> </div>
+                <div className="skill-frame">
+                    <div><img src={Html} alt={Html}/></div>
+                    <br/><h1>HTML</h1>
+                </div>
+
+                <div className="skill-frame">
+                    <div><img src={CSS} alt={CSS}/></div>
+                    <br/><h1>CSS</h1>
+                </div>
+
+                <div className="skill-frame">
+                    <div><img src={JavascriptIcon} alt={JavascriptIcon}/></div>
+                    <br/><h1>JAVASCRIPT</h1>
+                </div>
+
+                <div className="skill-frame">
+                    <div><img src={MySQL} alt={MySQL}/> </div>
+                    <br/><h1>MySQL</h1>
+                </div>
+
+                <div className="skill-frame">
+                    <div><img src={Figma} alt={Figma}/> </div>
+                    <br/><h1>MySQL</h1>
+                </div>
+
             </div>
 
+            <div className="tech-text-box-frame">
+                <motion.div variants={textVariant()}>
+                    <p className="Skills-text">MY SKILLS</p>
+                    <h2 className="Technologies-text">Technologies.</h2>
+                </motion.div>
+            </div>
         </div>
     );
 }
