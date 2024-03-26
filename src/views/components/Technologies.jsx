467baab046a3f4d8aabda75ffe3cfdf0f6fe2e27
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-26 23:51:33 +0100

    SKILLS update

--- src/views/components/Technologies.jsx
+++ src/views/components/Technologies.jsx
@@ -10,69 +10,64 @@ import {textVariant} from "../utils/motion";
 import REACT from '../../assets/svg/icons8-react-js.svg'
 import JAVA from '../../assets/svg/icons8-java.svg'
 import SPRINGBOOT from '../../assets/svg/icons8-spring-boot (1).svg'
+import Postman from '../../assets/svg/postman.svg'
+import Python from '../../assets/svg/icons8-python.svg'
 import frontend from "../../assets/svg/frontend.svg";
 
 
 function Technologies() {
     return (
         <div className="tech-main-frame">
+            <div className="tech-text-box-frame">
+                <motion.div variants={textVariant()}>
+                    <p className="Skills-text">MY SKILLS</p>
+                    <h2 className="Technologies-text">Technologies.</h2>
+                </motion.div>
+            </div>
             <div className="tech-skill-frame">
                 <div className="frontend-skill-frame">
                     <div className="skill-frame">
                         <div><img src={Html} alt={Html}/></div>
-                        <br/><h1>HTML</h1>
                     </div>
 
                     <div className="skill-frame">
                         <div><img src={CSS} alt={CSS}/></div>
-                        <br/><h1>CSS</h1>
                     </div>
 
                     <div className="skill-frame">
                         <div><img src={JavascriptIcon} alt={JavascriptIcon}/></div>
-                        <br/><h1>JAVASCRIPT</h1>
                     </div>
 
                     <div className="skill-frame">
                         <div><img src={REACT} alt={REACT}/></div>
-                        <br/><h1>REACT</h1>
                     </div>
 
                     <div className="skill-frame">
                         <div><img src={Figma} alt={Figma}/> </div>
-                        <br/><h1>MySQL</h1>
                     </div>
                 </div>
 
                 <div className="backend-skill-frame">
                     <div className="skill-frame">
                         <div><img src={JAVA} alt={JAVA}/> </div>
-                        <br/><h1>JAVA</h1>
+                    </div>
+
+                    <div className="skill-frame">
+                        <div><img src={Python} alt={Python}/> </div>
                     </div>
 
                     <div className="skill-frame">
                         <div><img src={SPRINGBOOT} alt={SPRINGBOOT}/> </div>
-                        <br/><h1>SPRING-BOOT</h1>
                     </div>
 
                     <div className="skill-frame">
                         <div><img src={MySQL} alt={MySQL}/> </div>
-                        <br/><h1>MySQL</h1>
                     </div>
 
                     <div className="skill-frame">
-                        <div><img src={MySQL} alt={MySQL}/> </div>
-                        <br/><h1>MySQL</h1>
+                        <div><img src={Postman} alt={MySQL}/> </div>
                     </div>
                 </div>
-
-            </div>
-
-            <div className="tech-text-box-frame">
-                <motion.div variants={textVariant()}>
-                    <p className="Skills-text">MY SKILLS</p>
-                    <h2 className="Technologies-text">Technologies.</h2>
-                </motion.div>
             </div>
         </div>
     );
