25f843863fe3a642c4ecef7282209fa78496a0de
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-27 20:05:48 +0100

    project page onset

--- src/views/components/Project.jsx
+++ src/views/components/Project.jsx
@@ -1,24 +1,32 @@
 import "../../styles/components/Project.css"
 import CocoaLogo from "../../assets/svg/cocoalate-project-logo.svg"
 import SKBLogo from "../../assets/svg/skb-project-logo.svg"
-import Link from "../../assets/svg/icons8-link.svg"
+import LinkIcon from "../../assets/svg/icons8-link.svg"
+import {Link} from "react-router-dom"
+
 
 const Project = ()=>{
     return (
         <div className="project-main-frame">
-            <p className="work-text"> MY RECENT WORK</p>
-            <h1 className="project-work-text">Here are a few projects I've worked on.</h1>
+            <div className="project-texts">
+                <p className="work-text"> MY RECENT WORK</p>
+                <h1 className="project-work-text">Here are a few projects I've worked on.</h1>
+            </div>
 
             <div className="project-cards">
                 <div className="cocoa-product">
                     <img  className="cocoa-logo" src={CocoaLogo} alt={CocoaLogo}/>
-                    <h1> A Simple Product Landing Page for an Hair Growth Oil Company</h1>
-                    <a><button><img src={Link} alt={Link}/></button></a>
+                    <h1 className="cocoa-logo-text"> A Simple Product Landing Page for an Hair Growth Oil Company</h1>
+                    <Link to={{pathname: "https://github.com/eniolaSamuel/CocoalateOil"}}>
+                        <img className="link-button" src={LinkIcon} alt={LinkIcon}/>
+                    </Link>
                 </div>
                 <div className="skb-backend">
                     <img  className="skb-logo" src={SKBLogo} alt={SKBLogo}/>
-                    <h1> An Apartment Booking Application for a Short-let Company </h1>
-                    <a><button><img src={Link} alt={Link}/></button></a>
+                    <h1 className="skb-logo-text"> An Apartment Booking Application for a Short-let Company </h1>
+                    <Link to={{pathname: "https://github.com/eniolaSamuel/skbHousing"}}>
+                        <img className="link-button" src={LinkIcon} alt={LinkIcon}/>
+                    </Link>
                 </div>
             </div>
 
