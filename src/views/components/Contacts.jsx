c9e319aae5c369c3c0912af21162d10b3f488153
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-30 23:30:38 +0100

    project page onset

--- src/views/components/Contacts.jsx
+++ src/views/components/Contacts.jsx
@@ -1,5 +1,6 @@
 import "../../styles/components/Contact.css"
 import LOGO from "../../assets/svg/monogram-logo.svg"
+import LINKEDIN from "../../assets/svg/black-linkedin-logo-15915.svg"
 
 const Contacts = () =>{
     return(
@@ -7,7 +8,12 @@ const Contacts = () =>{
             <div className="inner-frame">
                 <img src={LOGO} alt={LOGO}/>
                 <h1>"Logging out with pixels of passion, code in my heart,
-                    and dreams in my design. See you in the next click!"</h1>
+                    and dreams in my design. See you in the next click!"
+                </h1>
+
+                <div>
+
+                </div>
 
             </div>
         </div>
