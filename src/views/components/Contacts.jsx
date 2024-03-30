f3972939fa8ec1415eff36722c5848ff565a828e
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-30 23:02:06 +0100

    project page onset

--- src/views/components/Contacts.jsx
+++ src/views/components/Contacts.jsx
@@ -1,10 +1,11 @@
 import "../../styles/components/Contact.css"
+import LOGO from "../../assets/svg/monogram-logo.svg"
 
 const Contacts = () =>{
     return(
         <div className="contact-main-frame">
             <div className="inner-frame">
-
+                <img src={LOGO}/>
             </div>
         </div>
     )
